389c988210e1088f7748e1943411c49a759281e0
Harris Suhayb <harrissuhayb@gmail.com>
2020-02-25 20:29:11 +0500

    Add menu

---
+++ 2020-02-25/menu.py
@@ -0,0 +1,13 @@
+menu = [
+    'Input Items',
+    'Print',
+    'Average',
+    'Max/Min',
+    'Quit',
+]
+print("\n")
+for no, s in enumerate(menu):
+    print(
+        "{0}. {1:10s} ".format(no+1, s),
+        "\n")
+print("\n")
